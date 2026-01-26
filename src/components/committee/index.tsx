f8685e7af40b04d391c913835e8c501673f7d2a7
Rahim_Badsa <rahimbadsa723@gmail.com>
2026-01-26 15:18:00 +0600

    commitee page updated

--- src/components/committee/index.tsx
+++ src/components/committee/index.tsx
@@ -1,18 +1,23 @@
 import CentralCommitteeSection from '@components/shared/CentralCommitteeSection';
-import PageHero from '@/components/shared/PageHero';
+import Header from './../../components/layout/Header';
 import newsHero from '@assets/bg-img/committee-banner-img.png';
 import Footer from '@/components/layout/Footer';
 
 export default function CentralCommitteePage() {
    return (
       <div>
-         <PageHero
+         {/* <PageHero
             //             title='Committee'
             //             subtitle='The present committee was formed in a general meeting held on January 1, 2000. The meeting was chaired by Syed Sajjadul Karim Kabul and was attended by about 200 petrol pump station owners and various individuals. Syed Sajjadul Karim Kabul, Mir Ahsan Uddin Parvez and a 41-member panel are running the present committee. The following panel members are playing an important role in promoting the Bangladesh Petroleum Dealers, Distributors, Agents and Petrol Pump Owners Association throughout the country as the general secretaries of this association.
-            // '
+            // 'import Header from './../layout/Header';
+
             // backgroundImage={newsHero}
             height='compact'
-         />
+         /> */}
+         <Header/>
+         <div
+        className="my-12 md:my-24" 
+         ></div>
 
          <CentralCommitteeSection />
 
